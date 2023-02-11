89e70126018ea42d2aee448c4b87a0df36a1048e
frankieyeung <113824377+frankieyeung@users.noreply.github.com>
2023-02-11 17:39:43 +0800

    Add model

---
+++ decision_tree.py
@@ -0,0 +1,16 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+iris = datasets.load_iris()
+X= iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print('Target: ', y_test)
+print('Predict: ', y_pred)
+print('Accurate: ', model.score(X_test, y_test))
